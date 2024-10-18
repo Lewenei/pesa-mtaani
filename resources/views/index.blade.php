0dee234caa17c9b1d2a0cdca0758ca8d5d7ed684
Lewenei <brianngetich041@gmail.com>
2024-10-18 17:11:47 +0300

    Admin Dashboard: Admin can now land on the new dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.admin-layout')
+
+
+<main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Dashboard</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+        <div class="row">
+
+            <!-- Left side columns -->
+            <div class="col-lg-8">
+                <div class="row">
+
+                    <!-- Registered Products -->
+                    <div class="col-xxl-4 col-md-6">
+                        <div class="card info-card sales-card">
+                            <div class="card-body">
+                                <h5 class="card-title">Registered Products</h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-cart"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6></h6>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End Registered Products Card -->
+
+                    <!-- Users Card -->
+                    <div class="col-xxl-4 col-md-6">
+                        <div class="card info-card revenue-card">
+                            <div class="card-body">
+                                <h5 class="card-title">All Users</h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-person"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End Users Card -->
+
+
+
+                </div><!-- End Right side columns -->
+
+            </div>
+    </section>
+
+</main><!-- End #main -->
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,38 @@
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-house"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="">
+                <i class="bi bi-box"></i>
+                <span>Products</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="">
+                <i class="bi bi-people"></i>
+                <span>Suppliers</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="">
+                <i class="bi bi-geo-alt"></i>
+                <span>Warehouse Locations</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="bi bi-bag"></i> <!-- Sales icon -->
+                <span>Sales</span>
+            </a>
+        </li>
+    </ul>
+</aside>
